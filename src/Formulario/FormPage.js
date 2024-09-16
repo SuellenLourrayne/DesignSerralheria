6614a7a7a4b6948f374310b34ab33786ac5e2c3d
Bonnington Dublin <147234368+BonningtonDublin@users.noreply.github.com>
2024-09-16 21:07:52 +0100

    updates

--- src/Formulario/FormPage.js
+++ src/Formulario/FormPage.js
@@ -14,7 +14,7 @@ const FormPage = () => {
 
     // Prepara os dados do formulário para enviar ao WhatsApp
     const message = encodeURIComponent(`
-      Olá! Meu nome é ${name}.\nGostaria de orçamento para o serviço: ${service}. \nMais detalhes: ${details}`);
+      Olá! Meu nome é ${name}.\nGostaria de orçamento para o serviço: ${service}. \nMais detalhes: ${details}.`);
 
     const whatsAppUrl = `https://wa.me/353831938465?text=${message}`;
     window.open(whatsAppUrl, "_blank");
